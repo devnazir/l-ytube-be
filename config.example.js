969038f5569d58c1f6bee10585773892bd7583f3
devnazir <nazirbachrululum@gmail.com>
2021-07-26 17:04:44 +0700

    fix: Cannot find module './config'

--- config.example.js
+++ config.example.js
@@ -1,13 +1,15 @@
+require("dotenv").config({ path: ".env" });
+
 module.exports = {
   mongodb: {
-    Uri: "",
+    Uri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.mr9o4.mongodb.net:27017,cluster0-shard-00-01.mr9o4.mongodb.net:27017,cluster0-shard-00-02.mr9o4.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-11i50l-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
-      // useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
     },
   },
-  PORT: "", // number
-  secret: "",
+  PORT: 3004,
+  secret: process.env.JWT_SECRET,
 };
